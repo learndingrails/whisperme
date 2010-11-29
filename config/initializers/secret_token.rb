b24089ea3828e72bea7b87b7fa5ad35c80c9d971
Tristan <tristan@dynamicclarity.com>
2010-11-29 15:57:04 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whisperme::Application.config.secret_token = '8ef7aa4e10d7a0544869740b149864f5ee92eced5e58fefb9e8be3cad4af61ede6478ff3afe8f554825c8ccc6789a12d6ffe401eafce4afa7e900a8f0716f183'
